9813236acf146c9c4ccfac6567e14241c3c5a00e
Codehack-01 <ademolaprojectdreams@gmail.com>
2023-11-11 09:51:54 +0100

    source code

--- script.js
+++ script.js
@@ -9,20 +9,7 @@
 // let desksections = document.querySelectorAll('section');
 // let navLinks2 = document.querySelectorAll('header nav .desktop-nav a');
 
-// window.onscroll = () => {
-//    sections.forEach(sec => {
-//      let top = window.scrollY;
-//      let offset = sec.offsetTop - 150;
-//      let height = sec.offsetHeight;
-//      let id = sec.getAttribute('id');
-//      if(top >= offset && top < offset + height) {
-//        navLinks2.forEach(links => {
-//          links.classList.remove('active');
-//          document.querySelector('header nav .desktop-nav a[href*=' + id + ']').classList.add('active');
-//        });
-//      };
-//    });
-// };
+
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav .mobile-nav a, header nav .desktop-nav a');
@@ -38,7 +25,7 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav .mobile-nav a[href*=' + id + ']').classList.add('active');
                 document.querySelector('header nav .desktop-nav a[href*=' + id + ']').classList.add('active');
-                // document.querySelector('header nav .desktop-nav a[href*=' + id + ']').classList.add('active');
+                
             });
         }; 
         
