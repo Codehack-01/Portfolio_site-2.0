c3cec698cf301ce8e12dee57855aa03293f75d23
Codehack-01 <ademolaprojectdreams@gmail.com>
2023-11-16 04:15:01 +0100

    source code

--- script.js
+++ script.js
@@ -1,84 +1,24 @@
 
 
-// gsap.registerPlugin(ScrollTrigger)
-const splitTypes = document.querySelectorAll('.reveal-type')
-splitTypes.forEach((char, i) => {
-    const text = new SplitType(char, { types: 'chars' })
-    gsap.from(text.chars, {
-        opacity: 0.2,
-        stagger: 0.1,
-        scrollTrigger: {
-            trigger: char,
-            start: 'top 90%',
-            end: 'top 10%',
-            scrub: true,
-            markers: false,
-            toggleActions: 'play play play play',
-
-            onEnter: () => {
-              gsap.to(window, { scrollTo: { y: char, offsetY: 0 } });
-            },
-            
-        }
-    })
-})
-
-
-
-
-
 
 // gsap.registerPlugin(ScrollTrigger)
+
 // const splitTypes = document.querySelectorAll('.reveal-type')
-
-// splitTypes.forEach((char,i) => {
-//   const text = new SplitType(char, { types: 'chars'})
-
+// splitTypes.forEach((char, i) => {
+//   const text = new SplitType(char, { types: 'chars' })
 //   gsap.from(text.chars, {
-//     ScrollTrigger: {
-//       trigger: char,
-//       start: 'top 80%',
-//       end: 'top 20%',
-//       scrub: true,
-//       markers: false
-//     },
 //     opacity: 0.2,
-//     stagger: 0.1
+//     stagger: 0.1,
+//     scrollTrigger: {
+//       trigger: char,
+//       start: 'top 90%',
+//       end: 'top 10%',
+//       scrub: true,
+//       markers: false,
+//       toggleActions: 'play play play play',
+//     }
 //   })
 // })
-// splitTypes.forEach((char,i) => {
-//   const bg = char.dataset.bgColor
-//   const fg = char.dataset.fgColor
-//   const text = new SplitType(char, {types: 'chars'})
-//   gsap.fromTo(text.chars,
-//     {
-//       color: bg,
-//     },
-//     {
-//       color: fg,
-//       duration: 0.3,
-//       stagger: 0.02,
-//       ScrollTrigger: {
-//         trigger: char,
-//         start: 'top 80%',
-//         end: 'top 20%',
-//         scrub: true,
-//         markers: false,
-//         toggleActions: 'play play reverse reverse'
-//       }
-//   })
-// })
-
-
-
-
-// const NavLinksClose = document.querySelectorAll('#menu a')
-
-// NavLinksClose.forEach(link => {
-//   link.addEventListener('click', function() {
-//     document.getElementById('menu').style.display('none');
-//   });
-// });
 
 
 
@@ -103,17 +43,6 @@ window.onscroll = () => {
     });
 };
 
-// window.addEventListener('scroll', () => {
-//   const header = document.getElementById('navbar');
-//   if (window.scrollY > 0) {
-//     navbar.classList.remove('bg-transparent');
-//     navbar.classList.add('z-50', 'bg-black/70', 'backdrop-blur-md', 'backdrop-filter'); // Change to the desired background color
-//   } else {
-//     navbar.classList.remove('z-50 bg-black/70 backdrop-blur-md backdrop-filter');
-//     navbar.classList.add('bg-transparent');
-//   }
-// });
-
 
 const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
@@ -131,31 +60,6 @@ mobileNavLinks.forEach(link => {
   });
 });
 
-// btn.addEventListener('click', () => {
-//   btn.classList.toggle('open')
-//   nav.classList.toggle('flex')
-//   nav.classList.toggle('hidden')
-   
-// });
-
-// mobileNavLinks.forEach(link => {
-//   link.addEventListener('click', () => {
-//     nav.classList.add('hidden')
-//     btn.classList.remove('open')
-//   });
-// });
-
-
-// const NavLinksClose = document.querySelectorAll('#menu')
-
-// NavLinksClose.forEach(link => {
-//   link.addEventListener('click', function() {
-//     document.getElementById('menu').style.display('none');
-//   });
-// });
-
-
-
 
 window.addEventListener('scroll', () => {
     const navbar = document.getElementById('gradline');
@@ -169,70 +73,25 @@ window.addEventListener('scroll', () => {
 });
 
 
-// gsap.registerPlugin(ScrollTrigger)
-// const splitTypes = document.querySelectorAll('.reveal-type')
-// splitTypes.forEach((char,i) => {
-//   const bg = char.dataset.bgColor
-//   const fg = char.dataset.fgColor
-//   const text = new SplitType(char, {types: 'chars'})
-//   gsap.fromTo(text.chars,
-//     {
-//       color: bg,
-//     },
-//     {
-//       color: fg,
-//       duration: 0.3,
-//       stagger: 0.02,
-//       ScrollTrigger: {
-//         trigger: char,
-//         start: 'top 80%',
-//         end: 'top 20%',
-//         scrub: false,
-//         markers: false,
-//         toggleActions: 'play play reverse reverse'
-//       }
-//     })
-// })
+function initGSAPAnimation() {
+  gsap.registerPlugin(ScrollTrigger);
+  
+  const splitTypes = document.querySelectorAll('.reveal-type');
+  splitTypes.forEach((char, i) => {
+      const text = new SplitType(char, { types: 'chars' });
+      gsap.from(text.chars, {
+          opacity: 0.2,
+          stagger: 0.1,
+          scrollTrigger: {
+              trigger: char,
+              start: 'top 90%',
+              end: 'top 10%',
+              scrub: true,
+              markers: false,
+              toggleActions: 'play play play play',
+          }
+      });
+  });
+}
 
-// const lenis = new Lenis()
-// lenis.on('scroll', (e) => {
-//   console.log(e)
-// })
-// function raf(time) {
-//   lenis.raf(time)
-//   requestAnimationFrame(raf)
-// }
-// requestAnimationFrame(raf)
-
-
-
-
-
-
-// window.addEventListener('scroll', () => {
-//     const navbar = document.getElementById('navbar');
-//     if (window.scrollY > 0) {
-//       navbar.classList.remove('bg-transparent');
-//       navbar.classList.add('bg-black/70', 'backdrop-blur-md', 'backdrop-filter'); // Change to the desired background color
-//     } else {
-//       navbar.classList.remove('bg-black/70', 'backdrop-blur-md', 'backdrop-filter');
-//       navbar.classList.add('bg-transparent');
-//     }
-// });
-
-
-
-
-
-// var header = document.querySelector('navbar');
-// window.addEventListener('scroll', function () {
-//     if (window.pageYOffset > 100) {
-//         header.classList.add ('bg-black/70', 'backdrop-blur-md', ' backdrop-filter');
-//     } else {
-//         header.classList.remove();
-//     }
-// });
-
-//bg-black/70 backdrop-blur-md backdrop-filter
-
-//bg-gradient-radial
+initGSAPAnimation();
