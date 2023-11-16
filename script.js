ffa33c82bb652ec9bacad9213454740dcfd17430
Codehack-01 <ademolaprojectdreams@gmail.com>
2023-11-16 02:32:19 +0100

    source code

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.addEventListener('touchmove', function (e) {
+  e.preventDefault();
+});
+
 gsap.registerPlugin(ScrollTrigger)
 const splitTypes = document.querySelectorAll('.reveal-type')
 splitTypes.forEach((char, i) => {
@@ -7,8 +11,8 @@ splitTypes.forEach((char, i) => {
         stagger: 0.1,
         scrollTrigger: {
             trigger: char,
-            // start: 'top 80%',
-            // end: 'top 20%',
+            start: 'top 80%',
+            end: 'top 20%',
             scrub: true,
             markers: false,
             
